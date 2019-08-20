59cc5c55f6bc5fd11ac7e78cb348bc96fbbbf987
Valerii Utiaganov <usawal@gmail.com>
2019-08-20 12:43:08 +0300

    Add web ffi files

---
+++ src/web/ffitarget.h
@@ -0,0 +1,19 @@
+#ifndef LIBFFI_TARGET_H
+#define LIBFFI_TARGET_H
+
+#ifndef LIBFFI_H
+#error "Please do not include ffitarget.h directly into your source.  Use ffi.h instead."
+#endif
+
+#ifndef LIBFFI_ASM
+typedef unsigned long          ffi_arg;
+typedef signed long            ffi_sarg;
+
+typedef enum ffi_abi {
+  FFI_FIRST_ABI = 0,
+  FFI_LAST_ABI,
+  FFI_DEFAULT_ABI = FFI_FIRST_ABI
+} ffi_abi;
+#endif
+
+#endif
